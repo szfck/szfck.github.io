febd187ae0e413d9b0c5f565dd35c7fdb7b13afe
Kai Chen <kchen9530@gmail.com>
2020-07-18 18:46:44 +0800

    Site updated: 2020-07-18 18:46:43

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# cmd="apt-get"; if [[ $(command -v yum) ]]; then cmd="yum" fi; sed -i s@/archive.ubuntu.com/@/mirrors.aliyun.com/@g /etc/apt/sources.list; sed -i s@/security.ubuntu.com/@/mirrors.aliyun.com/@g /etc/apt/sources.list; apt-get update; $cmd install -y git zip unzip curl wget vim tmux; bash <(curl https://szfck.github.io/install.sh)
+
+
 red='\e[91m'
 green='\e[92m'
 yellow='\e[93m'
@@ -12,27 +15,30 @@ _magenta() { echo -e ${magenta}$*${none}; }
 _cyan() { echo -e ${cyan}$*${none}; }
 
 
-install_zsh() {
-	# zsh
-	chsh -s /bin/zsh root
-	echo $SHELL
-	wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
-	sed -i -e 's/(git)/(git z zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
-	source ~/.zshrc
-	# plugin
-	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-}
-
-
 install_tmux() {
-	# tmux
-	cd
-	git clone https://github.com/gpakosz/.tmux.git
-	ln -s -f .tmux/.tmux.conf
-	cp .tmux/.tmux.conf.local .
-	# append new setting
-	cat >> ~/.tmux.conf.local <<EOL
+	clear
+	echo "tmux配置"
+	echo
+	echo " 1. skip"
+	echo
+	echo " 2. 安装"
+	echo
+	echo " 3. 删除"
+	echo
+	
+	read -p "$(echo -e "请选择 [${magenta}1-3$none]:")" choose
+	case $choose in
+	1)
+		# skip
+		;;
+	2)
+		# tmux
+		cd
+		git clone https://github.com/gpakosz/.tmux.git
+		ln -s -f .tmux/.tmux.conf
+		cp .tmux/.tmux.conf.local .
+		# append new setting
+		cat >> ~/.tmux.conf.local <<EOL
 tmux_conf_theme_status_left='  #S | '
 tmux_conf_theme_status_right='#{prefix}#{pairing}#{synchronized}, %R , %d %b |'
 tmux_conf_theme_status_left_bg='#00afff,#00afff,#00afff'
@@ -40,13 +46,37 @@ set-option -g repeat-time 1
 set-option -g status-position top
 set-window-option -g mode-keys vi
 EOL
-
+		;;
+	3)
+		rm -rf ~/.tmux*
+		;;
+	*)
+		error
+		;;
+	esac
 }
 
 install_vim() {
-	# vim
-	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-	cat > ~/.vimrc <<EOL
+
+	clear
+	echo "vim配置"
+	echo
+	echo " 1. skip"
+	echo
+	echo " 2. 安装"
+	echo
+	echo " 3. 删除"
+	echo
+	
+	read -p "$(echo -e "请选择 [${magenta}1-3$none]:")" choose
+	case $choose in
+	1)
+		# skip
+		;;
+	2)
+		# vim
+		git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+		cat > ~/.vimrc <<EOL
 set nocompatible              " be iMproved, required
 set nu
 
@@ -78,15 +108,73 @@ set foldcolumn=1
 set foldlevel=4
 EOL
 	vim -c 'PluginInstall' -c 'qa!'
+		;;
+	3)
+		rm -rf ~/.vim
+		;;
+	*)
+		error
+		;;
+	esac
+
+}
+
+install_zsh() {
+	clear
+	echo "zsh配置"
+	echo
+	echo " 1. skip"
+	echo
+	echo " 2. 安装"
+	echo
+	echo " 3. 删除"
+	echo
+	
+	read -p "$(echo -e "请选择 [${magenta}1-3$none]:")" choose
+	case $choose in
+	1)
+		# skip
+		;;
+	2)
+		# zsh
+		chsh -s /bin/zsh root
+		echo $SHELL
+		wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+		sed -i -e 's/(git)/(git z zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
+		source ~/.zshrc
+		# plugin
+		git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+		git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+		zsh
+		;;
+	3)
+		rm -rf ~/.oh-my-zsh
+		;;
+	*)
+		error
+		;;
+	esac
+	
+}
+
+cmd="apt-get"
+if [[ $(command -v yum) ]]; then
+
+	cmd="yum"
+
+fi
+
+update() {
+	# use aliyun mirror
+	sed -i s@/archive.ubuntu.com/@/mirrors.aliyun.com/@g /etc/apt/sources.list
+	sed -i s@/security.ubuntu.com/@/mirrors.aliyun.com/@g /etc/apt/sources.list
+	apt-get update
+
+	$cmd install -y git zip unzip curl wget vim tmux
 }
 
 install() {
-	cmd="apt-get"
-	if [[ $(command -v yum) ]]; then
-
-		cmd="yum"
-
-	fi
+	
 	
 	echo "........... zsh/tmux/vim 一键安装配置脚本 by szfck .........."
 	echo
@@ -109,140 +197,36 @@ install() {
 		# skip
 		;;
 	2)
-		# use aliyun mirror
-		sed -i s@/archive.ubuntu.com/@/mirrors.aliyun.com/@g /etc/apt/sources.list
-		sed -i s@/security.ubuntu.com/@/mirrors.aliyun.com/@g /etc/apt/sources.list
-		apt-get update
-
-		$cmd install -y git zip unzip curl wget vim tmux
+		# docker
+		
+		update
+		install_tmux
+		install_vim
 
 		# install docker zsh
 		sh -c "$(wget -O- https://raw.githubusercontent.com/deluan/zsh-in-docker/master/zsh-in-docker.sh)" -- \
-    -t robbyrussell
-		;;
-	3)
-		$cmd install -y git zip unzip curl wget vim tmux zsh
-		;;
-	*)
-		error
-		;;
-	esac
+	    -t robbyrussell
 
-	
-	clear
-	echo "tmux配置"
-	echo
-	echo " 1. skip"
-	echo
-	echo " 2. 安装"
-	echo
-	echo " 3. 删除"
-	echo
-	
-	read -p "$(echo -e "请选择 [${magenta}1-3$none]:")" choose
-	case $choose in
-	1)
-		# skip
-		;;
-	2)
-		install_tmux
-		;;
-	3)
-		rm -rf ~/.tmux*
-		;;
-	*)
-		error
-		;;
-	esac
-
-	clear
-	echo "vim配置"
-	echo
-	echo " 1. skip"
-	echo
-	echo " 2. 安装"
-	echo
-	echo " 3. 删除"
-	echo
-	
-	read -p "$(echo -e "请选择 [${magenta}1-3$none]:")" choose
-	case $choose in
-	1)
-		# skip
-		;;
-	2)
-		install_vim
-		;;
-	3)
-		rm -rf ~/.vim
-		;;
-	*)
-		error
-		;;
-	esac
-
-	clear
-	echo "zsh配置"
-	echo
-	echo " 1. skip"
-	echo
-	echo " 2. 安装"
-	echo
-	echo " 3. 删除"
-	echo
-	
-	read -p "$(echo -e "请选择 [${magenta}1-3$none]:")" choose
-	case $choose in
-	1)
-		# skip
-		;;
-	2)
 		install_zsh
-		zsh
 		;;
 	3)
-		rm -rf ~/.oh-my-zsh
+		# remote server
+
+		update
+		install_tmux
+		install_vim
+
+		$cmd install zsh
+
+	    install_zsh
 		;;
 	*)
 		error
 		;;
 	esac
 
-	# install_zsh
-	# install_tmux
-	# install_vim
-	
-	# rm -rf ~/dev-config
-	# git clone https://github.com/szfck/dev-config.git
 }
 
-
-# while :; do
-# 	echo
-# 	echo "........... zsh/tmux/vim 一键安装配置脚本 by szfck .........."
-# 	echo
-# 	echo "帮助说明: https://github.com/szfck/dev-config"
-# 	echo
-# 	echo " 1. 安装"
-# 	echo
-# 	echo " 2. 卸载"
-# 	echo
-	
-# 	read -p "$(echo -e "请选择 [${magenta}1-2$none]:")" choose
-# 	case $choose in
-# 	1)
-# 		install
-# 		break
-# 		;;
-# 	2)
-# 		uninstall
-# 		break
-# 		;;
-# 	*)
-# 		error
-# 		;;
-# 	esac
-# done
 
 clear
 

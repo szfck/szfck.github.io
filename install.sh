b917f3f2a232ca2b9ecea38763d2453e42ceb42d
Kai Chen <kchen9530@gmail.com>
2020-07-18 13:36:11 +0800

    Site updated: 2020-07-18 13:36:10

--- install.sh
+++ install.sh
@@ -139,6 +139,7 @@ install() {
 	2)
 		# use aliyun mirror
 		sed -i s@/archive.ubuntu.com/@/mirrors.aliyun.com/@g /etc/apt/sources.list
+		sed -i s@/security.ubuntu.com/@/mirrors.aliyun.com/@g /etc/apt/sources.list
 		apt-get update
 
 		$cmd install -y git zip unzip curl wget vim tmux
